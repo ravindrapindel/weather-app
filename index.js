b2a2c27d02dbcda6e908d9602d54e764c47cd255
ravindrapindel <78081564+ravindrapindel@users.noreply.github.com>
2021-08-19 19:30:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+// key = "48cba28fbe0ba67197de313e7eca4189"
+// url= "http://api.weatherstack.com/"
+key ="2ab3fe01c1940330d27c19cda6ab0a0e";
+url = "https://api.openweathermap.org/data/2.5/weather?"
+
+
+const city = document.querySelector(".city");
+const date = document.querySelector(".date");
+const temp = document.querySelector(".temp");
+const weather = document.querySelector(".weather");
+const highlow = document.querySelector(".high-low");
+
+const searchbox = document.querySelector(".search-box");
+
+
+// console.log(temp, city, date, weather, highlow);
+async function fetchData(name) {
+ const ftch = await fetch(`${url}q=${name}&appid=${key}`);
+ const resp = await ftch.json();
+ city.innerText = resp.name;
+const date1 = new Date()
+date.innerText= date1.getDate() + "/" +date1.getMonth() +"/" + date1.getFullYear();
+temp.innerText = (parseInt(resp.main.temp) - 273.15).toFixed(2) + "°C";
+ weather.innerText = resp.weather[0].main;
+highlow.innerText =  (parseInt(resp.main.temp_min) - 273.15).toFixed(2) + "°C/" + (parseInt(resp.main.temp_max) - 273.15).toFixed(2) + "°C";
+ console.log(resp);
+}
+
+searchbox.onkeypress = (e)=>{
+    if(e.keyCode == 13){
+      fetchData(e.target.value);
+    }
+  }
